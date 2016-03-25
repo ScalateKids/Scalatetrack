bf8e3989a1a3e500f184fca90cf49c2fe5c15ff7
Andrea Giacomo Baldan <a.g.baldan@gmail.com>
2016-03-25 10:56:28 +0100

    Update sort

---
+++ controllers/controllers/requirements_controller.php
@@ -0,0 +1,71 @@
+<?php
+class RequirementsController {
+	public function index() {
+		if(!isset($_SESSION['logged'])) {
+			return call('pages', 'home');
+		} else {
+			$requirements = Requirement::all();
+			require_once('views/requirements/index.php');
+		}
+	}
+	public function add() {
+		if(!isset($_SESSION['logged'])) {
+			return call('pages', 'home');
+		} else {
+			$sources = Sources::all();
+			require_once('views/requirements/add.php');
+		}
+	}
+	public function save() {
+		if(!isset($_SESSION['logged'])) {
+			return call('pages', 'home');
+		} else {
+			$post_data = $_POST;
+			Requirement::add($post_data);
+			return call('requirements', 'index');
+		}
+	}
+	public function save_edit() {
+		if(!isset($_SESSION['logged'])) {
+			return call('pages', 'home');
+		} else {
+			if(!isset($_GET['code'])) {
+				return call('pages', 'error', 2);
+			}
+			$post_data = $_POST;
+			Requirement::save($post_data, $_GET['code']);
+			return call('requirements', 'index');
+		}
+	}
+	public function remove() {
+		if(!isset($_SESSION['logged'])) {
+			return call('pages', 'home');
+		} else {
+			if(!isset($_GET['code'])) {
+				return call('pages', 'error', 2);
+			}
+			Requirement::remove($_GET['code']);
+			return call('requirements', 'index');
+		}
+	}
+	public function alter() {
+		if(!isset($_SESSION['logged'])) {
+			return call('pages', 'home');
+		} else {
+			if(!isset($_GET['code'])) {
+				return call('pages', 'error', 2);
+			}
+			$sources = Sources::all();
+			$requirement = Requirement::get($_GET['code']);
+			require_once('views/requirements/alter.php');
+		}
+	}
+	public function show() {
+		if(!isset($_SESSION['logged'])) {
+			return call('pages', 'home');
+		} else {
+			require_once('views/requirements/show.php');
+		}
+	}
+}
+?>
